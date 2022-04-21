050119a6e21622c513aedcd5af059c841c778517
Elias Junior <elijah.junnior@outlook.com>
2022-04-21 12:14:10 -0300

    commenting SSR Auth validation function to improve your understanding

--- utils/WithSSRAuth.ts
+++ utils/WithSSRAuth.ts
@@ -14,13 +14,19 @@ type WithSSRAuthOptions = {
   roles?: string[]
 }
 
+// High Order Function usada para redi redirecionar usuario não autenticados ou autorizados 
 export function WithSSRAuth<T>(fn: GetServerSideProps<T>, options?: WithSSRAuthOptions) {
 
+  // retorna a função que será executada pelo Next antes da exibição das paginas com SSR
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<T>> => {
 
+    // Pega os cookies disponiveis para a pagina 
     const cookies = parseCookies(ctx);
+
+    // extra o token dos cookies
     const token = cookies['nextauth.token'];
 
+    // redireciona os usuário sem tokem para o login 
     if (!token) {
       return {
         redirect: {
@@ -30,16 +36,21 @@ export function WithSSRAuth<T>(fn: GetServerSideProps<T>, options?: WithSSRAuthO
       }
     }
 
+    //  verifica se a pagina exige alguma permissão
     if (options) {
 
+      // extrai as permissões necesárias para a pagina 
       const { permissions, roles } = options;
 
+      // decodifica, do token, as permissões que o usuário possui
       const user = decode<{ permissions: string[], roles: string[] }>(token);
 
+      // verifica se o usuário tem as permissões exigidas pela pagina 
       const userHasValidPermissions = validateUserPermissions({
         user, permissions, roles
       });
 
+      // redireciona os usuários não autorizados para a pagina DASHBOARD  
       if (!userHasValidPermissions) {
         return {
           redirect: {
@@ -53,15 +64,19 @@ export function WithSSRAuth<T>(fn: GetServerSideProps<T>, options?: WithSSRAuthO
 
     try {
 
+      // executa a função de SSR da pagina especifica
       return await fn(ctx);
 
     } catch (error) {
 
+      // trata os erros de autenticação
       if (error instanceof AuthTokenError) {
 
+        // destroi os cookies de autenticação
         destroyCookie(ctx, 'nextauth.token')
         destroyCookie(ctx, 'nextauth.refreshToken')
 
+        // redireciona o usuario para o login
         return {
           redirect: {
             destination: '/',
@@ -70,6 +85,9 @@ export function WithSSRAuth<T>(fn: GetServerSideProps<T>, options?: WithSSRAuthO
         }
 
       }
+
+      // caso seja outro erro encaminha o usuario para uma pagina de erro
+      // elias_fazer!!!
 
     }
 
